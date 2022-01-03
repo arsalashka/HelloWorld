0a682fc91a80d8440c075e5ef80bf09b68e71913
Arsalan Balzhinimaev <arsalashka@gmail.com>
2022-01-03 17:24:22 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Arsalan Balzhinimaev on 03.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
